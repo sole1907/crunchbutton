0be6d5bc5d283f662c04854f02a6e2d7a35dcd5a
ayansola_akanmu <ayansola_akanmu@mastercard.com>
2019-04-22 22:26:17 +0300

    check url path to determine if empty in post install script

--- cli/install.php
+++ cli/install.php
@@ -10,7 +10,8 @@ echo "\nInstalling...";
 
 $url = parse_url(getenv('DATABASE_URL'));
 
-if (!url || !$url['path']) {
+if (!$url || !$url['path']) {
+	echo "\nNo DATABASE_URL, trying to get JAWSDB_MARIA_URL";
 	$url = getenv('JAWSDB_MARIA_URL');
 }
 
